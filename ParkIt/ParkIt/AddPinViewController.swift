74923a4fd248c22ac34a4f71171bde5685aee997
AidanAllchin <aidan@allchin.com>
2018-02-28 17:11:51 -0800

    Added add spot

---
+++ ParkIt/ParkIt/AddPinViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AddPinViewController.swift
+//  ParkIt
+//
+//  Created by Aidan Allchin on 2/27/18.
+//  Copyright © 2018 ParkIt. All rights reserved.
+//
+
+import UIKit
+
+class AddPinViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
