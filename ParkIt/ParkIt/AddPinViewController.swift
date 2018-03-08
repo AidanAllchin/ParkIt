72bc44ebb8120682213dbd5b576073597f580521
AidanAllchin <aidan@allchin.com>
2018-03-08 09:03:44 -0800

    blah

--- ParkIt/ParkIt/AddPinViewController.swift
+++ ParkIt/ParkIt/AddPinViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class AddPinViewController: UIViewController {
-
-    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let contentsView : UIView = UIView()
+        var scrollView : UIScrollView = UIScrollView(frame: contentsView.frame)
+        
+        
+        let iv : UIButton = UIButton(frame: CGRect(x: 10, y: 10, width: 200, height: 200))
+        iv.setTitle("Hello", for: .normal)
+        self.view.addSubview(iv)
     }
 
     override func didReceiveMemoryWarning() {
