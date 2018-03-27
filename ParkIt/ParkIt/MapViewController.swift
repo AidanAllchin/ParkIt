43ddb6d7fe32bd689b424e8ac1ae68b620e67613
Will Austin <Willa19@lakesideschool.org>
2018-03-27 09:21:27 -0700

    Fixed Side Bar

--- ParkIt/ParkIt/MapViewController.swift
+++ ParkIt/ParkIt/MapViewController.swift
@@ -6,6 +6,7 @@ import Foundation
 
 
 class MapViewController: UIViewController, MKMapViewDelegate {
+    
     @IBOutlet var sideBarConstraint: NSLayoutConstraint!
     
     @IBAction func sideBarButtonPressed(_ sender: UIBarButtonItem) {
