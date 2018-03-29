12ba6aaf7f4c35abd38534b76ef8ba4bc1e9057c
williamfrohlich <william.frohlich@lakesideschool.org>
2018-03-29 09:11:45 -0700

    Revert "help"
    This reverts commit 11885b729767d83873f160f5ab53b3734c35b90e.

---
+++ MessagingApp/MessagingApp/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  MessagingApp
+//
+//  Created by Aidan Allchin on 2/2/18.
+//  Copyright © 2018 ParkIt. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ref:DatabaseReference?
+    var databaseHandle:DatabaseHandle?
+    
+    var postData = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //Set the firebase reference
+        ref = Database.database().reference()
+        
+        //Retrieve posts and listen for changes
+        databaseHandle = ref?.child("Posts").observe(.childAdded, with: { (snapshot) in
+            //Code to execute when child is added under "Posts"
+            //Take value from snapshot and add to postData array
+            
+            //Try to convert value of data to String
+            let post = snapshot.value as? String
+            
+            if let newPost = post {
+                //Append data to array
+                self.postData.append(newPost)
+                
+                //Reload tableView
+                self.tableView.reloadData()
+            }
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell")
+        cell?.textLabel?.text = postData[indexPath.row]
+        
+        return cell!
+    }
+
+
+}
+
---
+++ ParkIt/ParkIt/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ParkIt
+//
+//  Created by Aidan Allchin on 2/1/18.
+//  Copyright © 2018 ParkIt. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
