eb3e5c4edbc1cd0d5ff91e957c8f4ea1c02d7ee7
AidanAllchin <aidan@allchin.com>
2018-03-29 08:35:07 -0700

    Pre-logged-in startup implemented

--- ParkIt/ParkIt/AppDelegate.swift
+++ ParkIt/ParkIt/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         
         //checks to see if the user is logged in
         let def = UserDefaults.standard
@@ -24,13 +25,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         if userEmail != ""
         {
-            let mainController = MapViewController() //Create an instance of MapViewController
+            let mainController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "HomeScreen") as UIViewController //Create an instance of MapViewController
             
             window?.rootViewController = mainController //change the starting window to the MapViewController
             window?.makeKeyAndVisible() //show the window
         }
-        
-        FirebaseApp.configure()
+        else
+        {
+            
+        }
         return true
     }
 
