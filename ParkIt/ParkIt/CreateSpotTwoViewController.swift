eed51bb07a5b02fbc6b1dc06d9d9d665e353fd39
williamfrohlich <william.frohlich@lakesideschool.org>
2018-05-01 11:44:42 -0700

    WILLAFAFRAF

---
+++ ParkIt/ParkIt/CreateSpotTwoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CreateSpotTwoViewController.swift
+//  ParkIt
+//
+//  Created by Will Frohlich on 5/1/18.
+//  Copyright © 2018 ParkIt. All rights reserved.
+//
+
+import UIKit
+
+class CreateSpotTwoViewController: UIViewController {
+
+    var spot:ParkingSpot = ParkingSpot()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
