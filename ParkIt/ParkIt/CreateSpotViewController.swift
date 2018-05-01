149cde1666bd114f7b0839692c5459171e278cba
williamfrohlich <william.frohlich@lakesideschool.org>
2018-05-01 10:52:06 -0700

    WillF

--- ParkIt/ParkIt/CreateSpotViewController.swift
+++ ParkIt/ParkIt/CreateSpotViewController.swift
@@ -10,10 +10,12 @@ import Foundation
 import UIKit
 import MapKit
 
-class CreateSpotViewController: UIViewController, MKMapViewDelegate {
+class CreateSpotViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     let regionRadius: CLLocationDistance = 1000
+    var locationManager:CLLocationManager!
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +23,43 @@ class CreateSpotViewController: UIViewController, MKMapViewDelegate {
         mapView.delegate = self
         mapView.showsUserLocation = true
         mapView.userTrackingMode = MKUserTrackingMode(rawValue: 1)!
-        
-        centerMapOnLocation(location: mapView.userLocation)
+
     }
     
-    func centerMapOnLocation(location: CLLocation) {
-        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,regionRadius, regionRadius)
-        mapView.setRegion(coordinateRegion, animated: true)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        determineMyCurrentLocation()
     }
+    
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        
+        print("user latitude = \(userLocation.coordinate.latitude)")
+        print("user longitude = \(userLocation.coordinate.longitude)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
+    }
+    
+    
 }
