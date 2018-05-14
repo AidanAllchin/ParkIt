beceab1fea6568b566a6443580efe9700229e358
AidanAllchin <aidan@allchin.com>
2018-05-14 09:28:14 -0700

    Added Firebase/Functions to CocoaPods

--- ParkIt/functions/index.js
+++ ParkIt/functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
 
+admin.initializeApp(functions.config().firebase);
+const fb = admin.database()
+
+exports.getCurrentTime = functions.https.onRequest((request, response) => {
+	response.send(admin.database.ServerValue.TIMESTAMP);
+})
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
