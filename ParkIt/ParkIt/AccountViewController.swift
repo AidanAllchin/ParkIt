af05d66bca37e47b5576f75b55a78143b14f2c49
AidanAllchin <aidan@allchin.com>
2018-05-17 18:38:59 -0700

    changed account page

--- ParkIt/ParkIt/AccountViewController.swift
+++ ParkIt/ParkIt/AccountViewController.swift
@@ -13,6 +13,8 @@ class AccountViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usernameLabel.text = UserDefaults.standard.value(forKey: "userEmail") as? String
 
         // Do any additional setup after loading the view.
     }
@@ -21,16 +23,4 @@ class AccountViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
