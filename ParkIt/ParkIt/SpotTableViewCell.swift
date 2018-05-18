4aa9d78e5e5d1879cddcff18f9bf4dcdca099d67
williamfrohlich <william.frohlich@lakesideschool.org>
2018-05-18 09:44:13 -0700

    stuff git commit -m stuff

---
+++ ParkIt/ParkIt/SpotTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpotTableViewCell.swift
+//  ParkIt
+//
+//  Created by Will Frohlich on 5/18/18.
+//  Copyright © 2018 ParkIt. All rights reserved.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
