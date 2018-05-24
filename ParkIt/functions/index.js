8a359a39a127833b667b0ebad1e89d8e55500ca8
AidanAllchin <aidan@allchin.com>
2018-05-24 08:50:59 -0700

    AidanA

--- ParkIt/functions/index.js
+++ ParkIt/functions/index.js
@@ -1,11 +1,13 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
-const fb = admin.database()
+const fb = admin.database();
 
 exports.getCurrentTime = functions.https.onRequest((request, response) => {
-	response.send(admin.database.ServerValue.TIMESTAMP);
-})
+	const now = Date.now();
+	print(now);
+	response.send(now);
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
